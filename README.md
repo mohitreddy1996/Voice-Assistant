5f0e49e52a2ce38d1bb1cf9f4a488457c6e5c66b
Mohit Reddy <mohitreddy1996@gmail.com>
2017-01-18 03:06:05 +0530

    Basic Voice assistant done

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # Voice-Assistant
 Python based voice assistant using Google speech recognition and google text to speech modules.
+
+#### Run Following commands to
+* sudo apt-get install pyaudio
+* sudo apt-get install libportaudio-dev
+* sudo apt-get install python-dev
+* sudo apt-get install libportaudio0 libportaudio2 libportaudiocpp0 portaudio19-dev
+* sudo pip install SpeechRecognition
+* sudo pip install gTTS
+
+#### Technical Features
+* Background Listening to be implemented?
+
+#### Commands to be supported
+* Web Browser Control (Search)
+* Rhythmbox Controls (Play, Pause, Next, Previous, Stop)
+* Text/Code Editors (CLion, Webstorm, PyCharm, Android Studio, Gedit, Sublime)
+* YLC Controls (Play a movie, download subtitles using _subtitle downloader (personal project)_ ).
+* Open Terminal
--- main.py
+++ main.py
@@ -1,5 +1,55 @@
-import webbrowser
-import subprocess
+import os
 import speech_recognition as sr
 from gtts import gTTS as text_to_speech
+import subprocess
 
+webstorm_dir = "/home/mohit/WebStorm-145.972.4/bin/"
+pycharm_dir = "/home/mohit/pycharm-community-2016.1.2/bin/"
+idea_dir = "/home/mohit/idea-IC-145.597.3/bin/"
+
+
+def speak(audio_string):
+    tts = text_to_speech(text=audio_string, lang='en')
+    tts.save("audio.mp3")
+    os.system("mpg321 audio.mp3")
+
+listener = sr.Recognizer()
+
+
+def evaluate(response):
+    response = response.split(' ')
+    modified_response = []
+    for word in response:
+        word = str.lower(str(word))
+        modified_response.append(word)
+
+    print modified_response
+
+    if "javascript" in modified_response:
+        subprocess.Popen([webstorm_dir + "webstorm.sh"], shell=True)
+    elif "python" in modified_response:
+        subprocess.Popen([pycharm_dir + "pycharm.sh"], shell=True)
+    elif "java" in modified_response:
+        subprocess.Popen([idea_dir + "idea.sh"], shell=True)
+    elif "terminal" in modified_response:
+        os.system("gnome-terminal &")
+    else:
+        speak("Sorry, I didn't catch that.")
+
+while True:
+    with sr.Microphone() as recorder:
+        speak("Hi, How can I help you?")
+        user_response = listener.listen(recorder)
+
+    print "Processing Your Request"
+
+    try:
+        user_response_text = listener.recognize_google(user_response)
+        # speak("Did you just say " + user_response_text)
+        print user_response_text
+        evaluate(user_response_text)
+
+    except sr.UnknownValueError:
+        print("Google Speech Recognition could not understand audio")
+    except sr.RequestError as e:
+        print("Could not request results from Google Speech Recognition service; {0}".format(e))
